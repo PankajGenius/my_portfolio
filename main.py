641e7bbe8e0dd807fbd53778f6b24df1ff93c7b9
surya2402 <suryagupta654321@gmail.com>
2024-08-13 18:27:25 +0530

    Import pandas and render title from csv file

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import streamlit as st
+import pandas
+
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png", width= 200 )
+    st.image("images/pankaj.jpg", width=600)
 
 with col2:
     st.title("Pankaj")
@@ -16,3 +18,21 @@ with col2:
     I am confident that I can make meaningful contribution at your organisation. 
     """
     st.info(content)
+
+info = """
+Below you can find some of the apps. I have build in python. Feel free to contact me 
+"""
+st.write(info)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+
