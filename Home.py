480bd5d1f6a55032085721351436f1554e38156c
surya2402 <suryagupta654321@gmail.com>
2024-08-20 23:17:57 +0530

    created pages directed and contact us menu

--- Home.py
+++ Home.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/pankaj.jpg", width=600)
+    st.image("images/pankaj.jpg" )
 
 with col2:
     st.title("Pankaj")
@@ -31,8 +31,14 @@ df = pandas.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
