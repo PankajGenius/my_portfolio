dd8561a491c046e496cc11733688f7a704fb3600
surya2402 <suryagupta654321@gmail.com>
2024-08-25 00:47:55 +0530

    created email form then user can send mails

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,10 +1,18 @@
 import streamlit as st
+from semd_email import email_sending
 st.header("Contact Me ")
 
 with st.form(key="Your email"):
     user_email = st.text_input("Enter your email address")
-    email = st.text_area("Your message")
+    message = st.text_area("Your message")
+    message = f"""\
+Subject : New email from {user_email}
+ 
+From : {user_email}
+{message} 
+"""
     button = st.form_submit_button("Submit")
 
     if button:
-        print("Successfully submitted")
+        email_sending(message)
+        st.info("Message sent successfully")
--- semd_email.py
+++ semd_email.py
@@ -1,22 +1,18 @@
 import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "pan2kaj12@gmail.com"
-password = "solv ggaq osih qbbo"
+def email_sending(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "pan2kaj12@gmail.com"
+    password = "solv ggaq osih qbbo"
+    receiver = "pan2kaj12@gmail.com"
+    my_context = ssl.create_default_context()
 
-receiver = "pan2kaj12@gmail.com"
-my_context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=my_context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
-message = """/
-Hii !!
-My name is pankaj
-"""
-
-with smtplib.SMTP_SSL(host, port, context=my_context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
 
 
 
